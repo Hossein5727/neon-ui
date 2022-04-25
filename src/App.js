df48cc840ba4271282360ef3394554f895e9a2b0
Hossein <hosseinghiasi5727@gmail.com>
2022-04-25 06:24:36 -0700

    added files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { HomePage } from "./pages";
 
 function App() {
   return (
-    <div className="bg-black w-full h-[100vh]">
+    <div className="bg-black w-full h-[100%]">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,48 @@
 import avatarImg from "../assets/images/Avatar.svg";
 import Header from "../components/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 function HomePage() {
   return (
-    <div className="bggradinet border-none outline-none ">
-      <Header />
-      <div className="w-[full] flex justify-center items-center px-3 py-2">
-        <img className="" src={avatarImg} alt="avatar" />
-      </div>
+    <Swiper
+      modules={[Pagination]}
+      slidesPerView={1}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+      pagination={{ clickable: true }}
+      className="h-[100vh]"
+    >
+      {[1, 2, 3].map((item) => (
+        <SwiperSlide
+          className="bggradinet border-none outline-none "
+          key={item}
+        >
+          <Header />
+          <div className="w-[full] flex justify-center items-center px-3 py-2">
+            <img className="" src={avatarImg} alt="avatar" />
+          </div>
 
-      <div className="w-full flex justify-center items-center flex-col gap-4 mt-4 px-8">
-        <h2 className="font-[700] text-[34px] leading-[46px] tracking-[-0.3px] text-center textWhite">
-          Watch movies in Virtual Reality
-        </h2>
+          <div className="w-full flex justify-center items-center flex-col gap-4 mt-4 px-8">
+            <h2 className="font-[700] text-[34px] leading-[46px] tracking-[-0.3px] text-center textWhite">
+              Watch movies in Virtual Reality
+            </h2>
 
-        <p className="font-[400] text-[16px] leading-[22px] tracking-[-0.3px] text-center textWhite">
-          Download and watch offline wherever you are
-        </p>
+            <p className="font-[400] text-[16px] leading-[22px] tracking-[-0.3px] text-center textWhite">
+              Download and watch offline wherever you are
+            </p>
 
-        <button className="w-[160px] h-[41px] text-white rounded-lg bordergradient">
-          Sign Up
-        </button>
-      </div>
-    </div>
+            <button className="w-[160px] h-[41px] text-white rounded-lg bordergradient">
+              Sign Up
+            </button>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
 
